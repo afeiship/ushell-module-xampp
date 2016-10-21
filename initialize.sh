56c873a2646a82b61ae5c9436d92139e17a4225c
feizheng <1290657123@qq.com>
2016-10-21 23:07:00 +0800

    update url

--- initialize.sh
+++ initialize.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
-
+# http://pilotfiber.dl.sourceforge.net/project/xampp/XAMPP%20Linux/5.6.23/xampp-linux-x64-5.6.23-0-installer.run
+XAMPP_URL='https://www.apachefriends.org/xampp-files/5.6.24/xampp-linux-x64-5.6.24-1-installer.run';
 cd /tmp;
-wget http://pilotfiber.dl.sourceforge.net/project/xampp/XAMPP%20Linux/5.6.23/xampp-linux-x64-5.6.23-0-installer.run;
+wget $XAMPP_URL;
 chmod +x ./xampp-linux-x64-5.6.23-0-installer.run;
 ./xampp-linux-x64-5.6.23-0-installer.run;
