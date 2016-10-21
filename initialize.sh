5430e34f995c7ebadc285483174038fa82b733ae
feizheng <1290657123@qq.com>
2016-10-21 23:10:43 +0800

    udpate shell

--- initialize.sh
+++ initialize.sh
@@ -2,8 +2,8 @@
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
 # http://pilotfiber.dl.sourceforge.net/project/xampp/XAMPP%20Linux/5.6.23/xampp-linux-x64-5.6.23-0-installer.run
-XAMPP_URL='https://www.apachefriends.org/xampp-files/5.6.24/xampp-linux-x64-5.6.24-1-installer.run';
+XAMPP_URL=$1;
 cd /tmp;
 wget $XAMPP_URL;
-chmod +x ./xampp-linux-x64-5.6.23-0-installer.run;
-./xampp-linux-x64-5.6.23-0-installer.run;
+chmod +x ./*.run;
+./*.run;
