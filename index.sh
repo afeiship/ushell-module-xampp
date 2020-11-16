e86dfafdbc2e7da3f71c9de1ed8686251aaa9bf9
afeiship <1290657123@qq.com>
2020-11-16 08:20:05 +0800

    fix: remove old bower.json

--- index.sh
+++ index.sh
@@ -2,40 +2,6 @@
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
+# alias xxx='cd -';
 
-alias cd-www='cd /opt/lampp/htdocs';
-alias cd-root='cd /opt/lampp/htdocs';
-alias cd-config='cd /opt/lampp/etc';
-alias cd-padmin='cd /opt/lampp/phpmyadmin';
-alias e-padimin='vim /opt/lampp/phpmyadmin/config.inc.php';
-
-## initialize:
-alias xampp-install='$ROOT_PATH/initialize.sh';
-
-##default xampp:
-alias xampp='/opt/lampp/xampp';
-
-#Mysql
-alias mysql-start='/opt/lampp/xampp startmysql';
-alias mysql-stop='/opt/lampp/xampp stopmysql';
-alias mysql-reload='/opt/lampp/xampp reloadmysql';
-alias mysql-login='/opt/lampp/bin/mysql -uroot -p';
-alias mysql-status='/opt/lampp/bin/mysql.server status';
-
-#Apache
-alias apache-start='/opt/lampp/xampp startapache';
-alias apache-stop='/opt/lampp/xampp stopapache';
-alias apache-reload='/opt/lampp/xampp reloadapache';
-
-#FTP
-alias ftp-start='/opt/lampp/xampp startftp';
-alias ftp-stop='/opt/lampp/xampp stopftp';
-alias ftp-reload='/opt/lampp/xampp reloadftp';
-
-
-
-#VirtualHost
-alias e-vhost='vim /opt/lampp/etc/extra/httpd-vhosts.conf';
-alias e-xampp='vim /opt/lampp/etc/extra/httpd-xampp.conf';
-#xampp core conf file:
-alias e-httpd='vim /opt/lampp/etc/httpd.conf';
+unset ROOT_PATH;
--- initialize.sh
+++ initialize.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
-# http://pilotfiber.dl.sourceforge.net/project/xampp/XAMPP%20Linux/5.6.23/xampp-linux-x64-5.6.23-0-installer.run
-XAMPP_URL=$1;
-cd /tmp;
-wget $XAMPP_URL;
-chmod +x ./*.run;
-./*.run;
+
+# macos
+# ssh:
+/usr/bin/clear; ssh -i "$HOME/.bitnami/stackman/machines/xampp/ssh/id_rsa" -o StrictHostKeyChecking=no 'root@192.168.64.2'
+
+# mount xampp root
+cd $HOME/.bitnami/stackman/machines/xampp/volumes/root
+
+
+unset ROOT_PATH;
